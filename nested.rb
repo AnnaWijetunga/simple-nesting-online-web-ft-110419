b51c04b435d59e1b349005f0f8ac82f436b34dbc
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-09 19:40:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     array = programmer_hash[:dennis_ritchies][:languages]
+     array = programmer_hash[:dennis_ritchie][:languages]
      array.join
 end
 
